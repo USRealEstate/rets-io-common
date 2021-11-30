2dce8dfc7c43cbbc9b143ef6bb35877b92161369
YuCheng Hu <huyuchengus@gmail.com>
2021-11-30 11:15:43 -0500

    USRE-87 Add two new class for common util

---
+++ src/main/java/com/ossez/usreio/common/util/CaseInsensitiveComparator.java
@@ -0,0 +1,12 @@
+package com.ossez.usreio.common.util;
+
+import java.io.Serializable;
+import java.util.Comparator;
+
+public class CaseInsensitiveComparator<T> implements Comparator<T>, Serializable {
+	public int compare(Object o1, Object o2) {
+		String s1 = (String) o1;
+		String s2 = (String) o2;
+		return s1.compareToIgnoreCase(s2);
+	}
+}
---
+++ src/main/java/com/ossez/usreio/common/util/CaseInsensitiveTreeMap.java
@@ -0,0 +1,17 @@
+package com.ossez.usreio.common.util;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+
+public class CaseInsensitiveTreeMap<K, V> extends TreeMap<K, V> {
+	public CaseInsensitiveTreeMap(Map<K, V> map) {
+		this();
+		this.putAll(map);
+	}
+
+	public CaseInsensitiveTreeMap() {
+		super(new CaseInsensitiveComparator());
+	}
+
+}
